26c6138872c008a108a9938250bd46ccd0988253
imdaegeun <cd3818@naver.com>
2022-12-13 22:20:21 +0900

    xml 데이터 read batch

---
+++ src/main/java/com/example/springbatchpractice/job/apt/AptDealInsertJobConfig.java
@@ -0,0 +1,85 @@
+package com.example.springbatchpractice.job.apt;
+
+import com.example.springbatchpractice.core.dto.AptDealDto;
+import com.example.springbatchpractice.job.validator.FilePathParameterValidator;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.xml.StaxEventItemReader;
+import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class AptDealInsertJobConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job aptDealInsertJob(Step aptDealInsertStep) {
+        return jobBuilderFactory.get("aptDealInsertJob")
+                .incrementer(new RunIdIncrementer())
+                .validator(new FilePathParameterValidator())
+                .start(aptDealInsertStep)
+                .build()
+                ;
+    }
+
+    @Bean
+    @JobScope
+    public Step aptDealInsertStep(
+            StaxEventItemReader<AptDealDto> aptDealResourceReader
+            , ItemWriter<AptDealDto> aptDealWriter
+    ) {
+        return stepBuilderFactory.get("aptDealInsertStep")
+                .<AptDealDto, AptDealDto>chunk(10) // 1번째 제너릭 : 데이터를 읽을 타입, 2번재 제너릭 : 다음 프로세스로(여기서는 writer 부분) 데이터를 넘길 타입
+                .reader(aptDealResourceReader)
+                .writer(aptDealWriter)
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public StaxEventItemReader<AptDealDto> aptDealResourceReader(
+            @Value("#{jobParameters['filePath']}") String filePath
+            , Jaxb2Marshaller aptDealDtoMarshaller
+            ) {
+        return new StaxEventItemReaderBuilder<AptDealDto>()
+                .name("aptDealResourceReader")
+                .resource(new ClassPathResource(filePath))
+                .addFragmentRootElements("item")            // 각 데이터들의 root를 적음
+                .unmarshaller(aptDealDtoMarshaller)
+                .build()
+                ;
+    }
+
+    @Bean
+    @StepScope
+    public Jaxb2Marshaller aptDealDtoMarshaller() {
+        Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
+        jaxb2Marshaller.setClassesToBeBound(AptDealDto.class);
+        return jaxb2Marshaller;
+    }
+
+    @Bean
+    @StepScope
+    public ItemWriter<AptDealDto> aptDealWriter() {
+        return items -> {
+            items.forEach(System.out::println);
+        };
+    }
+
+}
