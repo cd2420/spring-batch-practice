382c9053d49e4720cc7afa202c9c41a577cb5fb3
imdaegeun <cd3818@naver.com>
2022-12-31 15:38:51 +0900

    lawd 코드 앞 5자리만 가져오는 sql 작성

--- src/main/java/com/example/springbatchpractice/core/repository/LawdRepository.java
+++ src/main/java/com/example/springbatchpractice/core/repository/LawdRepository.java
@@ -1,11 +1,22 @@
 package com.example.springbatchpractice.core.repository;
 
 import com.example.springbatchpractice.core.entity.Lawd;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LawdRepository extends JpaRepository<Lawd, Long> {
 
     Optional<Lawd> findByLawdCd(String lawdCd);
+
+    @Query(
+            "select distinct substr(l.lawdCd, 1, 5)" +
+                    "from lawd l" +
+                    "where l.exist = 1" +
+                    "and l.lawdCd not like '%00000000'"
+    )
+    List<String> findDistinctGuLawdCd();
+
 }
