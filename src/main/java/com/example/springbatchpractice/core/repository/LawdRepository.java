8ef964410d3e547f67c0e28293e317940410f16d
imdaegeun <cd3818@naver.com>
2023-01-01 23:54:43 +0900

    수정: Query를 jpa 모듈 클래스로 변경 	from lawd -> from Lawd 로 변경 : Entity 명과 일치 시킴

--- src/main/java/com/example/springbatchpractice/core/repository/LawdRepository.java
+++ src/main/java/com/example/springbatchpractice/core/repository/LawdRepository.java
@@ -1,8 +1,8 @@
 package com.example.springbatchpractice.core.repository;
 
 import com.example.springbatchpractice.core.entity.Lawd;
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,11 +12,7 @@ public interface LawdRepository extends JpaRepository<Lawd, Long> {
     Optional<Lawd> findByLawdCd(String lawdCd);
 
     @Query(
-            "select distinct substr(l.lawdCd, 1, 5)" +
-                    "from lawd l" +
-                    "where l.exist = 1" +
-                    "and l.lawdCd not like '%00000000'"
+            "select distinct substring(l.lawdCd, 1, 5) from Lawd l where l.exist = 1 and l.lawdCd not like '%00000000'"
     )
-    List<String> findDistinctGuLawdCd();
-
+    List<String> selectDistinctGuLawdCd();
 }
